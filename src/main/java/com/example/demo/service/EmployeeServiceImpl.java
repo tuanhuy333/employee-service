5bede6917b3f24ba88983153aad928e95559c1f0
NamQuang13 <dragonsang10a1@gmail.com>
2023-03-28 14:46:14 +0700

    Base Project

--- src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.demo.exception.EmployeeNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
